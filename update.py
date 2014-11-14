4aebe48f6747b346f903891dbea41723b9223312
Vasiliy Korchagin <vasiliy.korchagin@gmail.com>
2014-11-13 20:56:07 -0800

    Initial commit

---
+++ update.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+'''
+One message contains:
+    1. body
+    2. date
+    3. hasImg
+    4. hasUrl
+    5. host
+    6. id (good thing to use as a primary key)
+    7. nick
+    8. reg
+    9. title
+    10. topic
+'''
+
+import datetime
+import sqlite3
+import sys
+from suds.client import Client
+from suds import WebFault
+
+ZLO_ID = 0
+DB_NAME = 'board.db'
+
+SPEED_UPDATE_INT = 10
+
+conn = sqlite3.connect(DB_NAME)
+cur = conn.cursor()
+
+# Create table
+cur.execute('CREATE TABLE IF NOT EXISTS messages (body text, date timestamp,'
+            'hasImg boolean, hasUrl boolean, host text, id integer PRIMARY KEY,'
+            'nick text, reg boolean, title text, topic text)')
+
+c = Client('http://zlo.rt.mipt.ru:7500/ws/search?wsdl')
+service = c.service
+
+cur.execute('SELECT MAX(id) FROM messages')
+max_result = cur.fetchone()
+
+last_saved = 0 if max_result is None else max_result[0]
+last_indexed = service.getLastSavedMsgNumber(ZLO_ID)
+
+start_time = datetime.datetime.now()
+msg_per_sec = 0.0
+
+try:
+    for x in xrange(last_saved + 1, last_indexed + 1):
+        # Try again on fault.
+        msg = {}
+        fail_num = 0
+        while True:
+            try:
+                msg = service.getMessage(ZLO_ID, x)
+                break
+            except WebFault as wb:
+                fail_num += 1
+                if fail_num > 3:
+                    print "\rWebFault (#%d):" % x, wb.message
+                    break
+                continue
+            except Exception as e:
+                print "\rCannot parse message #%d:" % x, e.message
+                break
+        try:
+            cur.execute('INSERT INTO messages VALUES (?,?,?,?,?,?,?,?,?,?)',
+                        (msg.body, msg.date, msg.hasImg, msg.hasUrl, msg.host,
+                         msg.id, msg.nick, msg.reg, msg.title, msg.topic))
+        except AttributeError:
+            # Message doesn't exist
+            pass
+        if x % SPEED_UPDATE_INT == 0:
+            current_time = datetime.datetime.now()
+            delta = current_time - start_time
+            if delta.total_seconds() != 0:
+                msg_per_sec = (x - last_saved) / delta.total_seconds()
+        eta_sec = int((last_indexed - x) /
+                      (msg_per_sec if msg_per_sec != 0 else 0.01))
+        print '\r[%d/%d] Loading messages... (%.2f msg/sec, %d:%d:%d ETA)' % \
+            (x, last_indexed, msg_per_sec, eta_sec / 3600,
+                    (eta_sec % 3600) / 60, eta_sec % 60),
+        sys.stdout.flush()
+        if x % 1000 == 0:
+            conn.commit()
+finally:
+    conn.commit()
+    conn.close()
